095232250e937cbc754856abbcd0d58638dd8ce6
Kamran Sarwar <kamransarwar47@gmail.com>
2022-11-10 00:25:16 +0500

    resolvers.js file added
    Added resolvers.js file to handle query and mutation DB interaction and providing required response data.

---
+++ src/resolvers.js
@@ -0,0 +1,83 @@
+const { prisma } = require("./database.js");
+
+const Query = {
+    getAllPosts: (parent, args) => {
+        return prisma.post.findMany({
+            orderBy: [
+                {
+                    id: 'asc',
+                }
+            ],
+            include: {
+                category: true,
+                detail: true,
+            }
+        });
+    },
+};
+
+const Mutation = {
+    createPost: (parent, args) => {
+        const postArgs = args.post;
+        return prisma.post.create({
+            data: {
+                title: postArgs.title,
+                category: {
+                    connectOrCreate: {
+                        where: {
+                            name: postArgs.category.name,
+                        },
+                        create: {
+                            name: postArgs.category.name,
+                            isActive: postArgs.category.isActive
+                        }
+                    }
+                },
+                detail: {
+                    create: {
+                        text: postArgs.detail.text
+                    }
+                },
+            },
+            include: {
+                category: true,
+                detail: true,
+            }
+        });
+    },
+    updatePost: (parent, args) => {
+        const postArgs = args.post;
+        return prisma.post.update({
+            where: { id: Number(args.id) },
+            data: {
+                title: postArgs.title,
+                category: {
+                    connectOrCreate: {
+                        where: {
+                            name: postArgs.category.name,
+                        },
+                        create: {
+                            name: postArgs.category.name,
+                            isActive: postArgs.category.isActive
+                        }
+                    }
+                },
+                detail: {
+                    update: {
+                        text: postArgs.detail.text
+                    }
+                },
+            },
+            include: {
+                category: true,
+                detail: true,
+            }
+        });
+    },
+};
+
+const resolvers = { Query, Mutation };
+
+module.exports = {
+  resolvers,
+};
